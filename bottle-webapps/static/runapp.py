cbe59ad8e961ea18a7172b108f31d6a3c41d6759
amritagupta <gupta.amrita20@gmail.com>
2020-01-22 18:08:30 -0800

    import sys library for argparse

---
+++ bottle-webapps/static/runapp.py
@@ -0,0 +1,29 @@
+import argparse
+import sys
+import bottle
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run a web user interface for labeling camera trap images for classification.')
+    parser.add_argument('--host', type=str, default='0.0.0.0', help='Web server host to bind to.')## default='localhost', help='Web server host to bind to.')
+    parser.add_argument('--port', type=int, default=8080, help='Web server port port to listen on.')
+    args = parser.parse_args(sys.argv[1:])
+
+
+    # -------------------------------------------------------------------------------- #
+    # CREATE AND SET UP A BOTTLE APPLICATION FOR THE WEB UI
+    # -------------------------------------------------------------------------------- #
+    
+    webapp = bottle.Bottle()
+    webapp.add_hook("after_request", enable_cors)
+    webapp_server_kwargs = {
+        "server": "tornado",
+        "host": args.host,
+        "port": args.port
+    }
+
+    @webapp.route('/')
+    def index():
+        return bottle.static_file("index.html", root='static/html')
+
+    webapp.run(**webapp_server_kwargs)
